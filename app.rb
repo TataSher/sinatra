f3695e4c5211b02ecf9d473d6d03c8ac14808172
Natalia_Sherchenkova <tataterer@gmail.com>
2021-03-15 18:16:00 +0200

    Cat image added

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ get '/' do
 end
 
 get '/secret' do
-  "Whoop Whoop"
+  "Whoop Whoop - the name of my second book"
+end
+
+get '/secret/who_dat' do
+  "Noone. Move on!"
+end
+
+get '/cat' do
+  "<div>
+    <img src='https://i.natgeofe.com/n/9135ca87-0115-4a22-8caf-d1bdef97a814/75552.jpg?w=636&h=424'>
+   </div>"
 end
